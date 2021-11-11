acfaec8f44b661b41d5f71b92fd3c8bf58c9dc62
Eddy Gonzalezalez <edgonzalez@sufs.org>
2021-11-11 10:31:48 -0600

    Activity 1

--- src/magic-kingdom/activity1/Activity1.tsx
+++ src/magic-kingdom/activity1/Activity1.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React,{useState} from 'react';
 import styled from "styled-components";
+import image from './boat.png'
 
 const Styles = styled.div`
   color: red;
@@ -8,16 +9,37 @@ const Styles = styled.div`
     max-width: 900px;
     margin: 0 auto;
   }
+  
+  .border{
+	border:5px solid red;
+  }
 `;
 
+
+
 function Activity1() {
+
+const [riding,setRiding]=useState(true);
+
   return (
     <Styles>
       <div className="container">
-        Hello from Activity 1 Magic Kingdom
+       
       </div>
+	  
+	  {riding
+	  ?
+		<img src={image} alt="Boat" className="border"  />
+	  :
+	  <img src={image} alt="Boat" />
+	  }
     </Styles>
   )
+  
+  
 }
+
+
+
 
 export default Activity1
