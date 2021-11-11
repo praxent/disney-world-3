7d996fb79dba39528d2ed763748a4b760be0d324
Jose Pablo Solano <pabloso18@gmail.com>
2021-11-11 13:00:06 -0600

    Activity 2 JS epcot

--- src/epcot/activity2/Activity2.tsx
+++ src/epcot/activity2/Activity2.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from "styled-components";
+import './resources/styles/pagestyle.css';
+
+import restaurantImg from './resources/image/restaurantimg.jpeg';
 
 const Styles = styled.div`
   color: blue;
@@ -10,13 +13,58 @@ const Styles = styled.div`
   }
 `;
 
+
+
 function Activity2() {
+
+
+  const [data, setData]  = useState([])
+  useEffect(() => {
+      fetch("http://localhost:4000/space_220")
+      .then(response => response.json())
+      .then(res => setData(res));
+  }, [])
+
+  function changeItem(id) {
+ 
+    let items = data;
+
+    let item = data.filter(item => item.id === id);
+    let itemIndex = data.findIndex(item => item.id === id)
+
+    item.quantity = item.quantity - 1;
+
+    items[itemIndex] = item;
+  
+    setData({items});
+  }
+
   return (
-    <Styles>
-      <div className="container">
-        Hello from Activity 2 Epcot
+    <div style={{ backgroundImage: `url(${restaurantImg})`, height:"1200px", width:"2000px" } }>
+         
+
+    <div className="form-box">
+      <div className="header-text">
+        Restaurant
       </div>
-    </Styles>
+
+      
+      {data.map(( listValue, index ) => {
+          return (
+            <tr className="" key={index}>
+              <td> <button >{listValue.id}</button>  </td>
+              <td>{listValue.name}</td>
+              <td>{listValue.description}</td>
+              <td>{listValue.quantity}</td>
+            </tr>
+          );
+        })}
+
+
+
+	  </div>
+
+    </div>
   )
 }
 
