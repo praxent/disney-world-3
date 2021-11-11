166ae575959e7f53b77dfe07dc75c4b6681e997b
jxmoreno <73949866+jxmoreno@users.noreply.github.com>
2021-11-11 17:47:08 -0300

    activity2

--- src/blizzard-beach/activity2/Activity2.tsx
+++ src/blizzard-beach/activity2/Activity2.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import './Activity2.css'
+import React, { useState, useEffect } from 'react';
 import styled from "styled-components";
+import lottawattaLodge from "../images/Lottawatta-Lodge.jpg"
 
 const Styles = styled.div`
   color: blue;
@@ -11,10 +13,68 @@ const Styles = styled.div`
 `;
 
 function Activity2() {
+  const [menuArray, setData] = useState([]);
+
+  function buyAction(item){    
+
+    // menuArray.map(
+    //   row => {
+    //     if (row.id === item.id){
+    //       row.quantity = item.quantity -1;
+    //     }
+    //   }
+    // )
+    var currentItem = menuArray.filter(x=> x.id === item.id)[0];
+    currentItem.quantity = currentItem.quantity -1;
+    setData(menuArray);
+  }
+  useEffect(() => {
+    fetch('http://localhost:4000/lottawatta_lodge')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      setData(data);
+  })
+
+
+
+},[])
+
   return (
     <Styles>
-      <div className="container">
-        Hello from Activity 2 Blizzard Beach
+      <div className=".flex-row-activity2">
+        <div className="map-container">
+          <img src={lottawattaLodge} className="map"/>
+          <table className='productTable tablePosition'>
+            <thead>
+              <tr>
+                <th></th>
+                <th>Product</th>
+                <th>Description</th>
+                <th>For</th>
+                <th>Quantity</th>
+              </tr>
+            </thead>
+            <tbody>
+              {menuArray.map(item => {
+                return (
+                  <tr key={item.id}>
+                    <td>
+                      <input type='button' value='BUY' onClick={() => buyAction(item)}/>
+                      </td>
+                    <td>{ item.name }</td>
+                    <td className='descriptionColumn'>{ item.description }</td>
+                    <td>{ item.type }</td>
+                    <td>{ item.quantity }</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+
+          
+          </div>
+
       </div>
     </Styles>
   )
