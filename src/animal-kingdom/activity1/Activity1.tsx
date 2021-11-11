b57e5deff315f412512f54b9f97953399e201f25
Javier Moreno <jmoreno@sufs.org>
2021-11-11 14:26:51 -0300

    activity 1 Javier Moreno

--- src/animal-kingdom/activity1/Activity1.tsx
+++ src/animal-kingdom/activity1/Activity1.tsx
@@ -1,5 +1,10 @@
+import './Activity1.css'
 import React from 'react';
 import styled from "styled-components";
+import squirtTarget from "../images/fossil-fun-game-target.jpg";
+import waterAnimation from "../images/water-animation.gif";
+import { useState } from 'react';
+
 
 const Styles = styled.div`
   color: red;
@@ -8,13 +13,38 @@ const Styles = styled.div`
     max-width: 900px;
     margin: 0 auto;
   }
+
 `;
 
 function Activity1() {
-  return (
+  const [shootingIndicatorFlag, setShootingIndicator] = useState(true);
+  const [startShootingFlag, setShootingAnimation] = useState(false);
+  
+  
+  function handleShootingIndicator() {
+    setShootingIndicator(!shootingIndicatorFlag);    
+  }
+  function startShootingAnimation() {
+    if(shootingIndicatorFlag){
+      setShootingAnimation(shootingIndicatorFlag);   
+    }     
+  }
+
+  return (    
+
     <Styles>
-      <div className="container">
-        Hello from Activity 1 Animal Kingdom
+      <div className=".flex-row-activity1">
+        <div className="map-container">
+          <img src={squirtTarget} className="map"/>
+          <div className="labels">
+            <input type='button' onClick={handleShootingIndicator} className="label shootIndicator" value='Toogle Shooting'/>
+            <input type='button' className={shootingIndicatorFlag ? "label shootIndicatorFlag":"backgroundRed label shootIndicatorFlag"}  value={shootingIndicatorFlag ? 'Shoot at will' : 'Shooting not available'}/>
+            <input type='button' onClick={startShootingAnimation} className="activateShoot shootAnimation" value='Activate'/>
+
+            {startShootingFlag ? <img src={waterAnimation} className="waterImage" /> : null}
+          </div>
+          
+        </div>
       </div>
     </Styles>
   )
