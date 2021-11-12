d9fd36b61003cee71c033d702cb074545519f955
Jose Pablo Solano <pabloso18@gmail.com>
2021-11-11 23:45:47 -0600

    Activity-2 some changes

--- src/epcot/activity2/Activity2.tsx
+++ src/epcot/activity2/Activity2.tsx
@@ -16,28 +16,45 @@ const Styles = styled.div`
 
 
 function Activity2() {
+  const [data, setData]  = useState([]);
+  const [refreshKey, setRefreshKey] = useState(0);
 
-
-  const [data, setData]  = useState([])
   useEffect(() => {
+    function getData(){
       fetch("http://localhost:4000/space_220")
       .then(response => response.json())
       .then(res => setData(res));
-  }, [])
+    }
+    getData();
+  }, [refreshKey])
 
   function changeItem(id) {
  
-    let items = data;
+    var items = data;
 
-    let item = data.filter(item => item.id === id);
-    let itemIndex = data.findIndex(item => item.id === id)
+    var item = items.filter(item => item.id === id);
+    var itemIndex = items.findIndex(item => item.id === id)
 
-    item.quantity = item.quantity - 1;
+    item[0].quantity = item[0].quantity - 1;
 
-    items[itemIndex] = item;
+    items[itemIndex] = item[0];
   
-    setData({items});
+    setData(items);
+    setRefreshKey(oldKey => oldKey +1)
   }
+
+  const renderBody = () => {
+    return data && data.map(({ id, name, description, quantity }) => {
+        return (
+            <tr key={id}>
+                <td> <button onClick={() => changeItem(id)}>{id}</button>  </td>
+                <td>{name}</td>
+                <td>{description}</td>
+                <td>{quantity}</td>
+            </tr>
+        )
+    })
+}
 
   return (
     <div style={{ backgroundImage: `url(${restaurantImg})`, height:"1200px", width:"2000px" } }>
@@ -48,17 +65,13 @@ function Activity2() {
         Restaurant
       </div>
 
+      <table >
+      <tbody>
+          {renderBody()}
+      </tbody>
+      </table>
       
-      {data.map(( listValue, index ) => {
-          return (
-            <tr className="" key={index}>
-              <td> <button onClick={() => changeItem(listValue.id)}>{listValue.id}</button>  </td>
-              <td>{listValue.name}</td>
-              <td>{listValue.description}</td>
-              <td>{listValue.quantity}</td>
-            </tr>
-          );
-        })}
+
 
 
 
